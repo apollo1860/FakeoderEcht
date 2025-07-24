13d7324c526636aef8b52cf5da2fd065c252d1c5
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-07-24 14:09:48 +0200

    thesen.js erstellen

---
+++ thesen.js
@@ -0,0 +1,11 @@
+const realEvents = [
+  "Ein Mann überlebte einen Sprung aus einem Flugzeug ohne Fallschirm, weil er auf einem Ameisenhaufen landete.",
+  "Ein Schüler hackte das Schwarze Brett seiner Schule und sagte Unterricht im Juli wegen Schneefall ab.",
+  "Ein Paketbote warf ein Paket über den Zaun – es landete punktgenau in ein offenes Klo."
+];
+
+const fakeEvents = [
+  "In Schweden wurde ein Gesetz beschlossen, das tägliches Saunieren an Arbeitsplätzen erlaubt.",
+  "In Kanada wurde ein Elch offiziell zum Bürgermeister eines Dorfes gewählt – als Protestaktion.",
+  "Japan führte versuchsweise „Stille Züge“ ein, in denen nicht gesprochen werden darf."
+];
